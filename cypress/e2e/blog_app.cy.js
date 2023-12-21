f226b18bb5f191655decb1cdb8c9066d9ff7253a
MM <michele.makiki@gmail.com>
2023-12-21 13:48:22 +0200

    bloglist end to end testing, step2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
+    const user = {
+      name: 'Tatu Testaaja',
+      username: 'TaTe',
+      password: 'salainenSana'
+    }
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('http://localhost:5173')
   })
 
@@ -11,5 +17,22 @@ describe('Blog app', function () {
     cy.get('#username')
     cy.get('#password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('TaTe')
+      cy.get('#password').type('salainenSana')
+      cy.get('#login-button').click()
+
+      cy.contains('Tatu Testaaja logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('TaTe')
+      cy.get('#password').type('WrongPassword')
+      cy.get('#login-button').click()
+      cy.contains('wrong username or password')
+    })
   })
 })
